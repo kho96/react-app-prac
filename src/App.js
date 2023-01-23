bd11a3ad40a520a33a94ef4ea8f3ff23405439d5
kho96 <k____ho__@naver.com>
2023-01-24 03:12:09 +0900

    react basic 01/24

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Header(props) {
+  console.log('props', props, props.title);
+  return  <header>
+  <h1><a href="/" onClick={(event)=>{
+    event.preventDefault(); // 기본 event 막기
+    props.onChangeMod(); // 밑에서 정의한 함수 실행
+  }}>{props.title}</a></h1>
+</header>
+;
+}
+function Nav(props) {
+  const lis = [];
+  for(let i=0; i<props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+        <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+          event.preventDefault();
+          props.onChangeMod(event.target.id); //event.target - 이벤트 발생한 주체
+        }}>{t.title}</a>
+        </li>);
+  }
+  return <nav>
+  <ol>
+    {lis}
+  </ol>
+  </nav>
+  ;
+}
+function Article(props) {
+  return <article>
+  <h2>{props.title}</h2>
+  {props.body}
+</article>
+;
+}
 function App() {
+  const topics = [
+    {id: 1, title: "html", body: "html is ..."},
+    {id: 2, title: "css", body: "css is ..."},
+    {id: 3, title: "js", body: "js is ..."}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="WEB" onChangeMod={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMod={(id)=>{
+        alert(id);
+      }}></Nav>
+      <Article title="welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
